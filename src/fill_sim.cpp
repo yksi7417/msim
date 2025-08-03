14d30b3000bd676e600d08a1e5d7d12a1ad51fc0
Anthony Si <yatkasi@gmail.com>
2025-08-03 12:16:16 -0400

    first checkin

---
+++ src/fill_sim.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fix8/f8includes.hpp>
+#include <fix8/f8config.hpp>
+
+int main()
+{
+    std::cout << "[fill_sim] Running minimal FIX8 fill simulator..." << std::endl;
+    return 0;
+}
---
+++ tests/test_smoke.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Fix8 simulator smoke test passed.\n";
+    return 0;
+}
